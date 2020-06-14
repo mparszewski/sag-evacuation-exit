625240ebfb2bdbdc37b24d0637639ea1ca98d124
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2020-06-14 15:47:06 +0200

    refactor: new Java switch to vavr

--- src/main/java/com/infrastructure/Fire.java
+++ src/main/java/com/infrastructure/Fire.java
@@ -2,6 +2,7 @@ package com.infrastructure;
 
 import com.enums.Direction;
 import com.models.Point;
+import io.vavr.API;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +11,12 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+import static com.enums.Direction.*;
 import static com.infrastructure.Building.getBuilding;
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
 import static java.lang.Math.random;
+import static java.util.Objects.isNull;
 import static lombok.AccessLevel.NONE;
 
 @Getter
@@ -21,13 +26,6 @@ public class Fire implements PointListing {
     private Point startPoint;
     private CopyOnWriteArrayList<Point> allPoints;
 
-    @Getter(NONE)
-    private final Consumer<Point> RANDOM_SPREAD = point -> {
-        Stream.of(Direction.values())
-                .filter(d -> random() > 0.5)
-                .forEach(direction -> addPoint(point, direction));
-    };
-
     private Fire() {
         startPoint = getBuilding().getRandomAvailablePoint();
         allPoints = new CopyOnWriteArrayList<>();
@@ -36,9 +34,15 @@ public class Fire implements PointListing {
 
     public static Fire fire;
 
+    @Getter(NONE)
+    private final Consumer<Point> RANDOM_SPREAD = point -> {
+        Stream.of(Direction.values())
+                .filter(d -> random() > 0.5)
+                .forEach(direction -> addPoint(point, direction));
+    };
 
     public static Fire getFire() {
-        if (fire == null) {
+        if (isNull(fire)) {
             fire = new Fire();
         }
         return fire;
@@ -50,14 +54,13 @@ public class Fire implements PointListing {
     }
 
     private void addPoint(Point point, Direction direction) {
-        Point newPoint;
-        switch (direction) {
-            case UP -> newPoint = new Point(point.getX() - 1, point.getY());
-            case DOWN -> newPoint = new Point(point.getX() + 1, point.getY());
-            case LEFT -> newPoint = new Point(point.getX(), point.getY() - 1);
-            case RIGHT -> newPoint = new Point(point.getX() - 1, point.getY() + 1);
-            default -> newPoint = point;
-        }
+        Point newPoint = API.Match(direction).of(
+                Case($(UP), new Point(point.getX() - 1, point.getY())),
+                Case($(DOWN), new Point(point.getX() + 1, point.getY())),
+                Case($(LEFT), new Point(point.getX(), point.getY() - 1)),
+                Case($(RIGHT), new Point(point.getX() - 1, point.getY() + 1)),
+                Case($(), point)
+        );
         if (getBuilding().isPointAvailable(newPoint)) {
             allPoints.add(newPoint);
         }
